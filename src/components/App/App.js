46aeb29951098880beb8041e76c1b400f45cd982
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-02-21 18:17:31 +0200

    settings

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,10 +5,10 @@ import Filter from '../Filter';
 import ContactList from '../ContactsList';
 import s from './App.module.css';
 
-const DATA = [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }]
+// const DATA = [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }]
 
  
 
